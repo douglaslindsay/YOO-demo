5ecb7dc071f936f2bd1052338f84d68c15dfe4ee
Douglas Lindsay <douglasklindsay@gmail.com>
2025-04-25 15:52:58 +1200

    Add hamburger menu to navbar for small screen widths

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,6 @@
 <script>
     import logo from '$lib/assets/YOO_Logo_Transparent_Background.png';
+    let menu = false;
 </script>
 
 <style>
@@ -71,6 +72,35 @@
     a:hover {
         color: var(--text-active);
     }
+    button {
+        background-color: transparent;
+        border: none;
+        height: 100%;
+        aspect-ratio: 1 / 1;
+        display: none;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
+    .wrapper {
+        position: fixed;
+        top: 90px;
+        width: 100%;
+    }
+    .wrapper > nav {
+        flex-direction: column;
+        border: 3px solid rgba(139, 193, 139, 0.5);
+        border-radius: 15px;
+        margin-left: 5%;
+        margin-right: 5%;
+        backdrop-filter: blur(10px);
+        align-items: center;
+    }
+    .wrapper > nav > a {
+        padding: 10px;
+        width: 100%;
+        text-align: center;
+    }
+    .wrapper > nav { display: none; }
     @media screen and (max-width: 800px){
         .navbar {
             padding-left: 0;
@@ -78,21 +108,48 @@
         }
     }
     @media screen and (max-width: 650px){
-        nav {display: none;}
+        .navbar > nav {display: none;}
+        button { display: block;}
+        .wrapper > nav { display: flex; }
     }
 </style>
 
-<div class="navbar">
-    <div class="logo">
-        <img src={logo} alt="Logo"/>
-        <a href="/">Youth of Ōrākei</a>
+<div class="wrapper">
+    <div class="navbar">
+        <div class="logo">
+            <img src={logo} alt="Logo"/>
+            <a href="/">Youth of Ōrākei</a>
+        </div>
+
+        <nav>
+            <a href="/projects">Projects</a>
+            <a href="/members">Members</a>
+            <a href="/contact">Contact Us</a>
+            <a href="/history">History</a>
+        </nav>
+
+            <!-- todo: include lucide svg license-->
+        <button aria-label="menu" on:click={()=>(menu=!menu)}>
+            {#if menu}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x">
+                    <path d="M18 6 6 18"/>
+                    <path d="m6 6 12 12"/>
+                </svg>
+            {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu">
+                    <line x1="4" x2="20" y1="12" y2="12"/>
+                    <line x1="4" x2="20" y1="6" y2="6"/>
+                    <line x1="4" x2="20" y1="18" y2="18"/>
+                </svg>
+            {/if}
+        </button>
     </div>
-
-    <nav>
-        <a href="/projects">Projects</a>
-        <a href="/members">Members</a>
-        <a href="/contact">Contact Us</a>
-        <a href="/history">History</a>
-    </nav>
-
+    {#if menu}
+        <nav>
+            <a href="/projects">Projects</a>
+            <a href="/members">Members</a>
+            <a href="/contact">Contact Us</a>
+            <a href="/history">History</a>
+        </nav>
+    {/if}
 </div>
