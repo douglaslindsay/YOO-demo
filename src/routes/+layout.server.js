f399adcdb656b32fa8e55394fe4315ec8b08995d
Douglas Lindsay <douglasklindsay@gmail.com>
2025-05-03 14:48:09 +1200

    Make site work without .env

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -4,9 +4,14 @@ import { TOKEN, DATABASE_ID } from '$env/static/private';
 const notion = new Client({ auth: TOKEN });
 
 export async function load() {
-  const response = await notion.databases.query({
-    database_id: DATABASE_ID,
-  });
+  try {
+    const response = await notion.databases.query({
+      database_id: DATABASE_ID,
+    });
 
-  return { data: response.results }; // passed into data prop of pages (process individually in each of them)
+    return { data: response.results }; // passed into data prop of pages (process individually in each of them)
+  } catch (error) {
+    console.log("Error while fetching Notion in +layout.server.js");
+    return {};
+  }
 }
